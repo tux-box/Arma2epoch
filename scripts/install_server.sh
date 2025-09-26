09b2aa4bf17aa2fb8a554c3c32ef37558419b29d
tux-box <nathan.flow@gmail.com>
2025-09-26 11:55:30 -0500

    updated download links

--- scripts/install_server.sh
+++ scripts/install_server.sh
@@ -14,7 +14,7 @@ APP_ID_ARMA2_BAF=33930
 APP_ID_ARMA2_BETA=219540
 
 # DayZ Epoch Download URLs (replace with direct links if they expire)
-DAYZ_EPOCH_SERVER_URL="https://drive.google.com/uc?export=download&id=1jDn86sfTwcRae4NZgHK76k_CaY1jOUP2"
+DAYZ_EPOCH_SERVER_URL="https://drive.usercontent.google.com/download?id=1jDn86sfTwcRae4NZgHK76k_CaY1jOUP2&export=download&authuser=0&confirm=t&uuid=cb6e7fff-bfbd-405a-87ec-49c0be383ed9&at=AN8xHop3GRLPgRW9vGwJTjWerRji%3A1758904112974"
 DAYZ_EPOCH_SERVER_PASSWORD="123456"
 
 # Installation directory
@@ -40,7 +40,7 @@ rm dayz_epoch_server.7z
 
 # 3. Download and extract DayZ Epoch client files (for @dayz_epoch mod)
 echo "Downloading and extracting DayZ Epoch client..."
-DAYZ_EPOCH_CLIENT_URL="https://drive.google.com/uc?export=download&id=19iCJevU008g311vsxNR0PjYmkSv36YKv"
+DAYZ_EPOCH_CLIENT_URL="https://drive.usercontent.google.com/download?id=19iCJevU008g311vsxJNR0PjYmkSv36YK&export=download&authuser=0&confirm=t&uuid=a2e10166-8211-4f24-b7d7-7a80cbdd654b&at=AN8xHop414tzyCUH6oOVjSHuiYAS%3A1758904039424"
 wget -O dayz_epoch_client.7z "$DAYZ_EPOCH_CLIENT_URL"
 7z x dayz_epoch_client.7z -o$INSTALL_DIR/@dayz_epoch
 rm dayz_epoch_client.7z
